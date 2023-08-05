4176c709ec410851f94ebd5afc0f38692614e0a9
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2023-08-05 09:18:19 +0900

    Add files via upload

---
+++ gugudan.py
@@ -0,0 +1,7 @@
+﻿dan = int(input("단을 입력하세요."))
+startGop = int(input("시작 곱을 입력하세요."))
+endGop = int(input("끝 곱을 입력하세요."))
+start = startGop
+while start <= endGop:
+    print(f"{dan}  * {start} = {dan * start}")
+    start += 1
